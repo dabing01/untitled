c66a63a722e098b1353a928565cddefb51e68458
dabing01 <job25258@163.com>
2022-04-05 13:31:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+public class gitTest {
+    public static void main(String[] args){
+
+        System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
+    }
+}
